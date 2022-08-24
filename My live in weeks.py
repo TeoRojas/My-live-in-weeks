7a56f2f3a894c2e40aa197b5df4cf84bfeda5a20
TeoRojas <teofilo.rojas.mata@gmail.com>
2022-08-24 20:51:04 +0200

    modularised into functions

--- My live in weeks.py
+++ My live in weeks.py
@@ -2,49 +2,26 @@ import os
 from mliw_functions import *
 
 """
-1984-11-23
-a year has 52,143 weeks, 
+A year has 52,143 weeks, 
 so each block represents 1.00275 weeks.
 week = 0.99725754176 block.
 """
 
-years_to_live = 70
-blocks = 52
-lived = "◼"
-unlived = "◻"
+if __name__ == "__main__":
+    years_to_live = 70
+    blocks = 52
 
-birth_date = input("Enter your birthdate(yyyy-mm-dd) : ")
-birth = birth_date_to_dic(birth_date)
+    birth_date = input("Enter your birthdate(yyyy-mm-dd) : ")
+    birth = birth_date_to_dic(birth_date)
 
-days_lived = days_lived(birth['year'], birth['month'], birth['day'])
-weeks_lived = int(days_lived/7)
-blocks_lived = int(weeks_lived/1.00275)
-print(blocks_lived)
-os.system('clear')
+    days_lived = return_days_lived(birth['year'], birth['month'], birth['day'])
+    weeks_lived = int(days_lived/7)
+    blocks_lived = int(weeks_lived/1.00275)
 
-print("MY LIFE IN WEEKS".center(68), end='\n\n    ')
+    os.system('clear')
 
-for block in range(blocks+1):
-    if (block)%4 == 0 and block !=0:
-        print(str(block).rjust(4), end=' ')
+    print("MY LIFE IN WEEKS".center(68), end='\n\n    ')
 
-print()
+    prints_numeric_header(blocks)
 
-for year in range(years_to_live):
-    print(str(year).rjust(2), end = '  ')
-    for block in range(blocks):
-        if (block+1)%4 == 0 and block !=0:
-            if blocks_lived > 0:
-                print(lived + " ", end='')
-                blocks_lived = blocks_lived - 1
-            else:
-                print(unlived + " ", end='')
-        else:
-            if blocks_lived > 0:
-                print(lived, end='')
-                blocks_lived = blocks_lived - 1
-            else:
-                print(unlived, end='')   
-    print(" ")
-    if (year+1)%10 == 0:
-        print(" ")
+    prints_squares(years_to_live, blocks, blocks_lived)
--- mliw_functions.py
+++ mliw_functions.py
@@ -8,7 +8,7 @@ def birth_date_to_dic(birth_date):
     }
     return birth
 
-def days_lived(year, month, day):
+def return_days_lived(year, month, day):
     days_lived = int(str(
         datetime.datetime.now() - datetime.datetime(
             year=year, 
@@ -20,3 +20,32 @@ def days_lived(year, month, day):
 
     return days_lived
 
+def prints_numeric_header(blocks_number):
+    for block in range(blocks_number+1):
+        if (block)%4 == 0 and block !=0:
+            print(str(block).rjust(4), end=' ')
+
+    print()    
+
+def prints_squares(years_to_live, blocks_number, blocks_lived):
+    lived = "◼"
+    unlived = "◻"
+    
+    for year in range(years_to_live):
+        print(str(year).rjust(2), end = '  ')
+        for block in range(blocks_number):
+            if (block+1)%4 == 0 and block !=0:
+                if blocks_lived > 0:
+                    print(lived + " ", end='')
+                    blocks_lived = blocks_lived - 1
+                else:
+                    print(unlived + " ", end='')
+            else:
+                if blocks_lived > 0:
+                    print(lived, end='')
+                    blocks_lived = blocks_lived - 1
+                else:
+                    print(unlived, end='')   
+        print(" ")
+        if (year+1)%10 == 0:
+            print(" ")
